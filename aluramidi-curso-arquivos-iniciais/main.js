c00db204948cfdf216bed9a95111321bce1d0784
VikeVaporube <147057476+VikeVaporube@users.noreply.github.com>
2023-10-05 09:30:07 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,11 @@
+function tocaSomPom(){document.querySelector('#som_tecla_pom').play('');
+
+
+}
+
+
+const listaDeteclas document.querySelectorAll('.tecla');
+ listaDeteclas[0].onclick=tocaSomPom;
+ listaDeteclas[1].onclick=tocaSomClap;
+ listaDeteclas[2].onclick=tocaSomTim;
+ listaDeteclas[3].onclick=tocaSomPuff;
